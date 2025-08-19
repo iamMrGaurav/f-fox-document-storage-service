20f51d11e8d4dfe94247cdb7aead4285e48d4bd6
Gaurav Poudel <gaurair@Gauravs-MacBook-Air.local>
2025-08-19 11:50:15 +0530

    Project setup

---
+++ src/main/java/ai/freightfox/doc/storage/service/demo/FFoxDocStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package ai.freightfox.doc.storage.service.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FFoxDocStorageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FFoxDocStorageServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/freightfox/doc/storage/service/demo/FFoxDocStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.freightfox.doc.storage.service.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FFoxDocStorageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
